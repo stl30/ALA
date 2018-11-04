6f31b18731d9a04616b28919e922cd7add59e22f
Stoea Leontin <stoea.leontin@gmail.com>
2018-11-04 19:53:48 +0200

    no message

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,8 @@
  */
 
 // You can delete this file if you're not using it
+exports.modifyWebpackConfig = ({ config, stage }) => {
+    config.plugin("webpack-define", webpack.DefinePlugin, [{ "global.GENTLY": false }])
+
+    return config;
+};

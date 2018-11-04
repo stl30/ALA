4e0bd8bb13fd3ac867141872669d49276cffe29d
Stoea Leontin <stoea.leontin@gmail.com>
2018-11-04 19:25:17 +0200

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import dragula from 'react-dragula'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import { Link } from 'gatsby'
 import request from 'superagent'

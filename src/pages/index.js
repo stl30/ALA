ba86c30cde28906208c2618509539d3936573714
Stoea Leontin <stoea.leontin@gmail.com>
2018-11-04 11:47:37 +0200

    drag and boostrap

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,109 +1,113 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import ReactDOM from 'react-dom'
+import dragula from 'react-dragula'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import { Link } from 'gatsby'
 import request from 'superagent'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
+// import Image from '../components/image'
 
 var minusPoints = [];
 var plusPoints = [];
 var images = [{
-    "id" : "1", 
+    "id" : "1",
     "name"   : "child",
     "link" : "https://ae01.alicdn.com/kf/HTB1XGhfNXXXXXXDXFXXq6xXFXXX7/Kids-Puzzle-Educational-Toys-Disassembly-Assembly-Classic-Car-Model-Building-Toy-Children-best-gifts.jpg_640x640.jpg"
 },
 {
-    "id" : "2", 
+    "id" : "2",
     "name"   : "toy",
     "link" : "https://images-na.ssl-images-amazon.com/images/I/61JI4lEfY-L._SX355_.jpg"
 },
 {
-    "id" : "3", 
+    "id" : "3",
     "name"   : "child",
     "link" : "https://www.mouthsofmums.com.au/wp-content/uploads/2016/05/05/xtoys-shutterstock_321375317-mf.jpg.pagespeed.ic.wyiSRKl2DA.jpg"
 },
 {
-    "id" : "4", 
+    "id" : "4",
     "name"   : "teddy",
     "link" : "https://images-na.ssl-images-amazon.com/images/I/81QpshLNqeL._SL1500_.jpg"
 },
 {
-    "id" : "5", 
+    "id" : "5",
     "name"   : "child",
     "link" : "https://target.scene7.com/is/image/Target/GUEST_2ddd3f39-b150-4a2c-b7f8-7cc1b90c56e5?wid=488&hei=488&fmt=pjpeg"
 },
 {
-    "id" : "6", 
+    "id" : "6",
     "name"   : "car",
     "link" : "https://www.jeep-india.com/content/dam/cross-regional/apac/jeep/en_in/vehicle-lineup/jeep-Vehicle-Lineup-Compass.png"
 },
 {
-    "id" : "7", 
+    "id" : "7",
     "name"   : "bus",
     "link" : "https://upload.wikimedia.org/wikipedia/commons/6/6b/Transperth_Volgren_Optimus_bodied_Volvo_B8RLEA.jpg"
 },
 {
-    "id" : "8", 
+    "id" : "8",
     "name"   : "car",
     "link" : "https://www.sixt.com/fileadmin/files/global/user_upload/fleet/png/350x200/vw-beetle-2d-cabrio-rot-offen-2015.png"
 },
 {
-    "id" : "9", 
+    "id" : "9",
     "name"   : "truck",
     "link" : "https://img.grouponcdn.com/deal/cbt6NeGzUfzp5AZ6nNpUgVRGeHD/cb-2048x1229/v1/c700x420.jpg"
 },
 {
-    "id" : "10", 
+    "id" : "10",
     "name"   : "car",
     "link" : "https://www.nationwidevehiclecontracts.co.uk/m/1/dacia-logan-access.jpg"
 },
 {
-    "id" : "11", 
+    "id" : "11",
     "name"   : "cat",
     "link" : "https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
 },
 {
-    "id" : "12", 
+    "id" : "12",
     "name"   : "cute",
     "link" : "https://cdn.newsapi.com.au/image/v1/67a523605bca40778c6faaad93883a3b"
 },
 {
-    "id" : "13", 
+    "id" : "13",
     "name"   : "merino",
     "link" : "http://beaconalpacas.co.uk/wp-content/uploads/2016/11/Fabio.jpg"
 },
 {
-    "id" : "14", 
+    "id" : "14",
     "name"   : "wildlife",
     "link" : "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iJDLRm.3o2Iw/v0/800x-1.jpg"
 },
 {
-    "id" : "15", 
+    "id" : "15",
     "name"   : "mammal",
     "link" : "https://cdn.securesyte.com/qg5Pk2drzv-991/images/blog/raccoon-in-missouri.png"
 },
 {
-    "id" : "16", 
+    "id" : "16",
     "name"   : "panoramic",
     "link" : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7-Nd73d_Ii8JMWSufxAynVjyTKWLfCBTWf2DsBGQVCq3o8tN2"
 },
 {
-    "id" : "17", 
+    "id" : "17",
     "name"   : "grass",
     "link" : "https://images.pexels.com/photos/60006/spring-tree-flowers-meadow-60006.jpeg?cs=srgb&dl=nature-flowers-sun-60006.jpg"
 },
 {
-    "id" : "18", 
+    "id" : "18",
     "name"   : "snow",
     "link" : "https://mountaincountieswater.com/wp-content/uploads/2011/04/2013-Snow-Sierras-2.jpg"
 },
 {
-    "id" : "19", 
+    "id" : "19",
     "name"   : "sunset",
     "link" : "http://wall2born.com/data/out/345/image-44351151-wallpapers-view-nature-hd.jpg"
 },
 {
-    "id" : "20", 
+    "id" : "20",
     "name"   : "desert",
     "link" : "https://defenders.org/sites/default/files/styles/homepage-feature-2015/public/mojave-desert_mendenhall-glacier_jason-mohap.png"
 }];
@@ -155,9 +159,9 @@ function checkCateg(cetegorySelected){
 
 function toggleButtons(option){
     document.getElementById("cat1button").disabled = option;
-    document.getElementById("cat2button").disabled = option; 
-    document.getElementById("cat3button").disabled = option; 
-    document.getElementById("cat4button").disabled = option; 
+    document.getElementById("cat2button").disabled = option;
+    document.getElementById("cat3button").disabled = option;
+    document.getElementById("cat4button").disabled = option;
 }
 
 
@@ -216,34 +220,64 @@ function getUrlList() {
 }
 
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby application. Press start to begin :)</p>
-    <div id="score"></div>
+export default class IndexPage extends React.Component {
+    componentDidMount() {
+        // var container = ReactDOM.findDOMNode(this);
+        var drag = document.getElementsByClassName('container')[0];
+        var drag1 = document.getElementsByClassName('container')[1];
+        // console.log(document.getElementsByClassName('container'));
+        dragula([drag, drag1]);
+    }
+    render(){
+        return (
+              <Layout>
+                <h1>Hi people</h1>
+                <p>Welcome to your new Gatsby application. Press start to begin :)</p>
+                <div className='container'>
+                    <div className='btn btn-warning'>Swap me around</div>
+                    <div>Swap her around</div>
+                    <div>Swap him around</div>
+                    <div>Swap them around</div>
+                    <div>Swap us around</div>
+                    <div>Swap things around</div>
+                    <div>Swap everything around</div>
+                </div>
+                  <h1>List</h1>
+                  <div className='container'>
+                    <div>Swap me around</div>
+                    <div>Swap her around</div>
+                    <div>Swap him around</div>
+                    <div>Swap them around</div>
+                    <div>Swap us around</div>
+                    <div>Swap things around</div>
+                    <div>Swap everything around</div>
+                </div>
+                <div id="score"></div>
 
-    <button id="startbutton" onClick={callLoop}>Start</button>
+                <button id="startbutton" onClick={callLoop}>Start</button>
 
-    <div id="appcontent"  style={{display: 'none'}}>
-        <div style={{width: '48%', float: 'left'}}>
-            <div>This is the item</div>
-            <div><img id="currentIMG" src="" width="250" /></div>
+                <div id="appcontent"  style={{display: 'none'}}>
+                    <div style={{width: '48%', float: 'left'}}>
+                        <div>This is the item</div>
+                        <div><img id="currentIMG" src="" width="250" /></div>
 
-            <button id="cat1button" onClick={() => checkCateg("animals")}>Animals</button>
-            <button id="cat2button" onClick={() => checkCateg("nature")}>Nature</button>
-            <button id="cat3button" onClick={() => checkCateg("vehicles")}>Vehicles</button>
-            <button id="cat4button" onClick={() => checkCateg("toys")}>Toys</button>
-        </div>
-        <div style={{width: '48%', float: 'right'}}>
-            <pre id="response"></pre>
-        </div>
-    </div>
-    
-
-    <div id="response_val" style={{visibility: 'hidden'}}></div>
-
-  </Layout>
-)
+                        <button id="cat1button" onClick={() => checkCateg("animals")}>Animals</button>
+                        <button id="cat2button" onClick={() => checkCateg("nature")}>Nature</button>
+                        <button id="cat3button" onClick={() => checkCateg("vehicles")}>Vehicles</button>
+                        <button id="cat4button" onClick={() => checkCateg("toys")}>Toys</button>
+                    </div>
+                    <div style={{width: '48%', float: 'right'}}>
+                        <pre id="response"></pre>
+                    </div>
+                </div>
 
 
-export default IndexPage
+                <div id="response_val" style={{visibility: 'hidden'}}></div>
+
+              </Layout>
+        )
+    }
+}
+
+
+// export default IndexPage

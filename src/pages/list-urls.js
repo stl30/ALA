9b95869ff40fec03869f21c268a0e540812de504
Stoea Leontin <stoea.leontin@gmail.com>
2018-11-04 04:21:37 +0200

    lista urluri/ adaugare/ stergere url

--- src/pages/list-urls.js
+++ src/pages/list-urls.js
@@ -6,6 +6,20 @@ import Layout from '../components/layout'
 import Image from '../components/image'
 
 
+function addDeleteListeners() {
+    var deleteButtons = document.getElementsByClassName('delete-url');
+    for (var i = 0; i < deleteButtons.length; i++) {
+        // console.log(deleteButtons[i]);
+        // deleteButtons[i].addEventListener("click", deleteUrl(deleteButtons[i]));
+        deleteButtons[i].addEventListener("click", function (e) {
+            // console.log(this.dataset['id'])
+            deleteUrl(this.dataset['id']);
+        });
+    }
+}
+
+
+
 function getUrlList() {
     request
         .post('https://fcc--alae.herokuapp.com/v1alpha1/graphql')
@@ -21,15 +35,89 @@ function getUrlList() {
                 }
                 return -1;
             }).forEach(function (e) {
-                urls += '<p>'+e['photo_url']+'</p>'
+
+                urls += '<tr>' +
+                    '       <td>'+e['id']+'</td>' +
+                    '       <td>'+e['photo_url']+'</td>' +
+                    '       <td><button  class="delete-url"  data-id="'+e['id']+'">delete</button></td>' +
+                    '</tr>'
             });
 
-            console.log(urls)
-            var list = document.getElementById('list-urls');
+            // console.log(urls)
+            var list = document.getElementById('urls_table_body');
             list.innerHTML = urls;
+
+            addDeleteListeners();
 
         });
 }
+
+function addPhotoLink(photoUrl) {
+    var imgUrl = document.getElementById('url_photo_input');
+    console.log(imgUrl.value)
+
+    request
+        .post('https://fcc--alae.herokuapp.com/v1alpha1/graphql')
+        .send({
+            "query": "mutation insert_photo_url {insert_photos_urls(objects: [{photo_url: \"" + imgUrl.value + "\",}]) {returning {id photo_url}}}",
+            "variables": null,
+            "operationName": "insert_photo_url"
+        }) // sends a JSON post body
+        .set('X-API-Key', 'foobar')
+        .set('accept', 'json')
+        .end((err, res) => {
+            // Calling the end function will send the request
+            // var photosUrlsRandom = JSON.parse(res.text).data.photos_urls.sort(function (e) {
+            //     if (Math.random() > .5) {
+            //         return 1
+            //     }
+            //     return -1;
+            // });
+            // var photosUrlsRandom = JSON.parse(res.text).data.photos_urls.sort(function (e) {
+            //     if (Math.random() > .5) {
+            //         return 1
+            //     }
+            //     return -1;
+            // });
+
+            console.log(res)
+
+
+        });
+    imgUrl.value = '';
+    getUrlList();
+}
+
+function deleteUrl(urlId) {
+    request
+        .post('https://fcc--alae.herokuapp.com/v1alpha1/graphql')
+        .send({"query":"mutation delete_url_with_id {delete_photos_urls(where: {id: {_eq: "+urlId+"}}) {affected_rows}}","variables":null,"operationName":"delete_url_with_id"}) // sends a JSON post body
+        .set('X-API-Key', 'foobar')
+        .set('accept', 'json')
+        .end((err, res) => {
+            // Calling the end function will send the request
+            // var photosUrlsRandom = JSON.parse(res.text).data.photos_urls.sort(function (e) {
+            //     if (Math.random() > .5) {
+            //         return 1
+            //     }
+            //     return -1;
+            // });
+            // var photosUrlsRandom = JSON.parse(res.text).data.photos_urls.sort(function (e) {
+            //     if (Math.random() > .5) {
+            //         return 1
+            //     }
+            //     return -1;
+            // });
+
+            console.log(res)
+            getUrlList();
+
+        });
+}
+
+    //init
+    getUrlList();
+
 
 
 const IndexPage = () => (
@@ -38,9 +126,26 @@ const IndexPage = () => (
         <p>Welcome to your new Gatsby application. Press start to begin 1:)</p>
 
         <button id="list-urls" onClick={getUrlList}>urls</button>
+        <input id='url_photo_input' type="text" name='url_photo'/>
+        <button id="add-urls" onClick={addPhotoLink}>add url</button>
 
+        <table >
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>url</th>
+                    <th>delete</th>
+                </tr>
+            </thead>
 
-        <div id="list-urls"></div>
+            <tbody id="urls_table_body">
+
+            </tbody>
+        </table>
+
+        <div id="list-urls">
+
+        </div>
 
     </Layout>
 )
